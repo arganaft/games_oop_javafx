110803387d675d5d3ab8e818e0efd16cd145ccef
arganaft <e1mar.arganaft@gmail.com>
2022-12-19 13:28:43 +0300

    2. Сокобан. Тестовое задание.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,44 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        return (isColumnAssembled(board) || isLineAssembled(board));
+    }
+
+    public static boolean isLineAssembled (int[][] board) {
+        boolean result = false;
+        for (int row = 0; row < board.length; row++) {
+            for (int cell = 0; cell < board.length; cell++) {
+                if (board[row][cell] == 0) {
+                    result = false;
+                    break;
+                }
+                if (board[row][board.length - 1] == 1) {
+                    result = true;
+                }
+            }
+            if (result){
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean isColumnAssembled (int[][] board) {
+        boolean result = false;
+        for (int cell = 0; cell < board.length; cell++) {
+            for (int row = 0; row < board.length; row++) {
+                if (board[row][cell] == 0) {
+                    result = false;
+                    break;
+                }
+                if (board[board.length - 1][cell] == 1) {
+                    result = true;
+                }
+            }
+            if (result){
+                break;
+            }
+        }
+        return result;
     }
 }
