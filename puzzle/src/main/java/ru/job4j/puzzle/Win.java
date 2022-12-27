b8782e2ebcecd6ba403d85cf83afed5a27a1ea7d
arganaft <e1mar.arganaft@gmail.com>
2022-12-27 18:58:13 +0300

    1.1.7. Контрольные вопросы и тестовое задание. / 2. Сокобан. Тестовое задание.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,44 +2,35 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        return (isColumnAssembled(board) || isLineAssembled(board));
-    }
-
-    public static boolean isLineAssembled(int[][] board) {
         boolean result = false;
-        for (int row = 0; row < board.length; row++) {
-            for (int cell = 0; cell < board.length; cell++) {
-                if (board[row][cell] == 0) {
-                    result = false;
-                    break;
-                }
-                if (board[row][board.length - 1] == 1) {
-                    result = true;
-                }
-            }
-            if (result) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (isColumnAssembled(board, i) || isLineAssembled(board, i))) {
+                result = true;
                 break;
             }
         }
         return result;
     }
 
-    public static boolean isColumnAssembled(int[][] board) {
-        boolean result = false;
-        for (int cell = 0; cell < board.length; cell++) {
-            for (int row = 0; row < board.length; row++) {
-                if (board[row][cell] == 0) {
+    public static boolean isLineAssembled(int[][] board, int row) {
+        boolean result = true;
+            for (int column = 0; column < board.length; column++) {
+                if (board[row][column] == 0) {
                     result = false;
                     break;
                 }
-                if (board[board.length - 1][cell] == 1) {
-                    result = true;
+            }
+        return result;
+    }
+
+    public static boolean isColumnAssembled(int[][] board, int column) {
+        boolean result = true;
+            for (int row = 0; row < board.length; row++) {
+                if (board[row][column] == 0) {
+                    result = false;
+                    break;
                 }
             }
-            if (result) {
-                break;
-            }
-        }
         return result;
     }
 }
