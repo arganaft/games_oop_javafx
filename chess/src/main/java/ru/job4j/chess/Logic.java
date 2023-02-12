fae7e8bb0d32bdbb17e8efc137b6781339dc05fc
arganaft <e1mar.arganaft@gmail.com>
2023-02-13 02:32:54 +0300

    1.2.8. Контрольные вопросы 2. Каркас шахматной доски

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -21,7 +21,7 @@ public final class Logic {
     }
 
     private boolean free(Cell[] steps) throws OccupiedCellException {
-        for (Figure figure : figures){
+        for (Figure figure : figures) {
             for (Cell step : steps) {
                 if (figure.position().equals(step)) {
                     throw new OccupiedCellException();
